3af57f69fc28636057d62222c750254da53c1502
fkruger-anderson <101761088+fkruger-anderson@users.noreply.github.com>
2022-08-28 19:01:29 -0300

    Update Universidade.java

--- Universidade.java
+++ Universidade.java
@@ -1,9 +1,11 @@
-package entities;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.averagingDouble;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  *
@@ -28,54 +30,88 @@ public class Universidade {
     public String getCNPJ() {
         return CNPJ;
     }
-    
+
     public Curso criarCurso(String nome, String dataCriacao, String descricao) {
         Curso curso = new Curso(++geradorCurso, nome, dataCriacao, descricao);
         cursos.add(curso);
         return curso;
     }
-    
+
     public Optional<Curso> buscarCurso(int codigo) {
         return cursos.stream().filter(c -> c.getCodigo() == codigo).findFirst();
     }
-    
+
     public boolean adicionarProfessor(int codCurso, String dataAdmissao, String nome, String dataNascimento, String CPF) {
         Curso curso = this.buscarCurso(codCurso).orElse(null);
         if (curso != null) {
             return curso.getProfessores().add(new Professor(curso, dataAdmissao, nome, dataNascimento, CPF));
         } else {
-            System.out.println("Curso n„o encontrado nesta Universidade");
+            System.out.println("Curso n√£o encontrado nesta Universidade");
             return false;
         }
     }
-    
+
     public boolean criarTurma(int codCurso, String nome, int qtdVagas, String matriculaProf) {
         Curso curso = this.buscarCurso(codCurso).orElse(null);
         Professor prof = curso.buscarProfessor(matriculaProf).orElse(null);
-        
-        if (curso != null && prof != null) {
+
+        if (prof != null) {
             return curso.addTurma(nome, qtdVagas, prof);
         } else {
-            System.out.println("Dados n„o encontrados nesta Universidade.");
+            System.out.println("Dados n√£o encontrados nesta Universidade.");
             return false;
         }
     }
-    
+
     public boolean matricularAluno(int codCurso, int codTurma, Aluno aluno) {
         Curso curso = this.buscarCurso(codCurso).orElse(null);
         Turma turma = curso.buscarTurma(codTurma).orElse(null);
-        
-        if (curso != null && turma != null) {
+
+        if (turma != null) {
             if (turma.getAlunos().size() + 1 <= turma.getQtdVagas() && !turma.getAlunos().contains(aluno)) {
                 return turma.getAlunos().add(aluno);
             } else {
-                System.out.println("Excedeu os limites da turma, ou o aluno j· encontra-se matriculado.");
+                System.out.println("Excedeu os limites da turma, ou o aluno j√° encontra-se matriculado.");
                 return false;
             }
         } else  {
-            System.out.println("Dados n„o encontrados nesta Universidade.");
+            System.out.println("Dados n√£o encontrados nesta Universidade.");
             return false;
         }
+    }
+
+    public boolean registrarNotaAluno(int codCurso, int codTurma, String matricula, Double nota) {
+        Curso curso = this.buscarCurso(codCurso).orElse(null);
+        Turma turma = curso.buscarTurma(codTurma).orElse(null);
+        Aluno aluno = turma.buscarAluno(matricula).orElse(null);
+
+        if (aluno != null) {
+            turma.getNotas().put(String.format("%s - %s", aluno.getMatricula(), aluno.getNome()), nota);
+            return true;
+        } else {
+            System.out.println("Dados n√£o encontrados nesta Universidade.");
+            return false;
+        }
+    }
+
+    public void calcMediaAlunos(int codCurso, int codTurma) {
+        Curso curso = this.buscarCurso(codCurso).orElse(null);
+        Turma turma = curso.buscarTurma(codTurma).orElse(null);
+        Map<String, Double> notas = turma.getNotas();
+
+//        Map<String,Double> average =
+//                notas.entrySet().stream()
+//                     .collect(Collectors.groupingBy(Map.Entry::getKey,
+//                             Collectors.averagingDouble(value -> value.getValue())));
+        Map<String,Double> average =
+                notas.entrySet().stream()
+                     .collect(groupingBy(Map.Entry::getKey,
+                             averagingDouble(value -> value.getValue())));
+
+        for (Map.Entry<String, Double> entry : average.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
     }
 
     @Override
